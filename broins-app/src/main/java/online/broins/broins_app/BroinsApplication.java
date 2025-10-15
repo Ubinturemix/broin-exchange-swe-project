51743d8f3444bc0f04e3401fca82957241be6865
Ubinturemix <edgarperez8005@gmail.com>
2025-10-14 20:49:26 -0500

    Initial commit of broins-app

---
+++ broins-app/src/main/java/online/broins/broins_app/BroinsApplication.java
@@ -0,0 +1,13 @@
+package online.broins.broins_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BroinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BroinsApplication.class, args);
+	}
+
+}
---
+++ broins-app/src/test/java/online/broins/broins_app/BroinsApplicationTests.java
@@ -0,0 +1,13 @@
+package online.broins.broins_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BroinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
